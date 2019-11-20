e67906933a21f073d49e3afa1136c6a1e89383d4
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-20 01:38:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,15 +1,15 @@
 class Owner
-  attr_accessor :name, :species
+  attr_reader :name, :species
   @@all = []
 
-  def initialize(owner_name)
+  def initialize(name)
     @name = name
-    @species = 'human'
+    @species = "human"
     @@all << self
   end
 
   def say_species
-    'I am a #{@species}.'
+    "I am a #{@species}."
   end
 
   def self.all
@@ -26,10 +26,39 @@ class Owner
 
   def cats
     Cat.all.select{|cat| cat.owner == self}
-end
+  end
 
   def dogs
     Dog.all.select{|dog| dog.owner == self}
   end
- end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each {|doggo| doggo.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|kitty| kitty.mood = "happy"}
+  end
+
+  def sell_pets
+    pets = self.dogs + self.cats
+    pets.each do |pet|
+      pet.owner = nil
+      pet.mood = "nervous"
+    end
+
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
 end
