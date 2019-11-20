0f7469af46a9ab39cb8c3708d022075856da3d79
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-20 01:39:23 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = Owner.new(owner).name
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
   # code goes here
 end
