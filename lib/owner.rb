474241eea2c81488a043b65c8119b7dc1f7c4ee5
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-20 01:20:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,9 +5,13 @@ class Owner
     @name = name
   end
 
+  def set_name(owner)
+    @name = owner
+  end
+
   def get_name
     return @name
   end
 end
 
-owner = owner.new("victoria")
+@name = owner.new
