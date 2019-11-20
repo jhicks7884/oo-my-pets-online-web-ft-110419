f9b9bc72da6385cf6837ebcb2643e1c5481dc512
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-20 01:13:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,9 @@
 class Owner
-
+  
   attr_accessor :name
 
   def initialize(owner_name)
-     @owner
+    @name = name
   end
-end
-
-  def get_name
-    return @owner
-  end
-  # code goes here
-end
+  
+  
